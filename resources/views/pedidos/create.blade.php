439b69338ad1aa17f6232be16840d4e02d73d528
Your Name <leticia_rodriguesdesousa@hotmail.com>
2022-10-01 12:15:11 -0300

    Projeto Livraria

---
+++ resources/views/pedidos/create.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<title>Cadastrar Novo Pedido</title>
+</head>
+
+<body>
+	<form action="">
+		<label>Descricao</label>
+		<input type="text" name="descricao">
+		<label>Data do Pedido</label>
+		<input type="date" name="dataPedido">
+		<label>Valor</label>
+		<input type="number" name="valor">
+		<label>Quantidade</label>
+		<input type="number" name="quantidade">
+		<button>Salvar</button>
+	</form>
+
+</body>
+
+</html>
